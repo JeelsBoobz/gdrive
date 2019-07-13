730ce2f163b5da1fe2420b5c62a2c723ef942c17
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2019-07-13 09:06:22 +0700

    Update about.go

--- drive/about.go
+++ drive/about.go
@@ -22,7 +22,12 @@ func (self *Drive) About(args AboutArgs) (err error) {
 
 	fmt.Fprintf(args.Out, "User: %s <%s>\n", user.DisplayName, user.EmailAddress)
 	fmt.Fprintf(args.Out, "Used: %s\n", formatSize(quota.Usage, args.SizeInBytes))
-	fmt.Fprintf(args.Out, "Free: %s\n", formatSize(quota.Limit-quota.Usage, args.SizeInBytes))
+	if quota.Limit-quota.usage < 0 {
+		fmt.Fprintf(args.Out, "Free: %s\n", "-")
+	} else {
+		fmt.Fprintf(args.Out, "Free: %s\n", formatSize(quota.Limit-quota.Usage, args.SizeInBytes))
+	}
+	fmt.Fprintf(args.Out, "Total_test: %s\n", quota.Limit)
 	fmt.Fprintf(args.Out, "Total: %s\n", formatSize(quota.Limit, args.SizeInBytes))
 	fmt.Fprintf(args.Out, "Max upload size: %s\n", formatSize(about.MaxUploadSize, args.SizeInBytes))
 	return
