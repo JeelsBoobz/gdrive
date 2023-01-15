6752f8e2cf9e156cba3261f44dd09c3758fae1bd
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2023-01-15 11:52:56 +0700

    Update build-all.sh

--- _release/build-all.sh
+++ _release/build-all.sh
@@ -3,7 +3,7 @@
 APP_NAME="gdrive"
 PLATFORMS="darwin/386 darwin/amd64 darwin/arm darwin/arm64 dragonfly/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips64 linux/mips64le linux/rpi netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 openbsd/arm plan9/386 plan9/amd64 solaris/amd64 windows/386 windows/amd64"
 
-BIN_PATH="_release/bin"
+BIN_PATH="./_release/bin"
 
 # Initialize bin dir
 mkdir -p $BIN_PATH
